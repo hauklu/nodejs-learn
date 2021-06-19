fbe68b7cf97bf8a1fe3caf22a8818f16dd844635
luchanghao <765160768@qq.com>
2021-06-19 16:28:21 +0800

    .

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 8888
 const path = require('path')
 
-app.use((req, res) => {
-  res.json({
-    name: 'zhangsan'
-  })
-})
+// app.use((req, res) => {
+//   res.json({
+//     name: 'zhangsan'
+//   })
+// })
 
 // // app.use(express.static(path.join(__dirname, 'public')))
 // app.use(express.static(path.join(__dirname)))
@@ -16,6 +16,18 @@ app.use((req, res) => {
 // app.get('/', (req, res) => {
 //   res.send('hello 1')
 // })
+
+app.get('/name/:age', (req, res) => {
+  let { age } = req.params
+  res.json({
+    name: 'tom',
+    age
+  })
+})
+
+app.post('/name', (req, res) => {
+  res.send('tom post')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
